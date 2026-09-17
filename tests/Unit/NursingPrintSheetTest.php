7439388791530ad9ac72b7fb92511f99963d5265
echuquillanqui <151475247+echuquillanqui@users.noreply.github.com>
2026-09-17 13:34:17 -0500

    Dejar solo nombres en firmas de hemodiálisis

--- tests/Unit/NursingPrintSheetTest.php
+++ tests/Unit/NursingPrintSheetTest.php
@@ -13,4 +13,11 @@ class NursingPrintSheetTest extends TestCase
         $this->assertStringContainsString('FUNCIONES VITALES', $view);
         $this->assertStringNotContainsString('EXAMEN FISICO', $view);
     }
+
+    public function test_print_sheet_only_shows_professional_names_in_signature_lines(): void
+    {
+        $view = file_get_contents(resource_path('views/atenciones/enfermeria/_print_sheet.blade.php'));
+
+        $this->assertSame(4, substr_count($view, "'showDetails' => false"));
+    }
 }
